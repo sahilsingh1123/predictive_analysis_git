24af588a1dcc7b41a835172f9ec68b57f8bc3847
sahilsingh1123 <sahilsingh1123@gmail.com>
2019-03-22 10:02:58 +0530

    printed the response data

--- entry_predictive.py
+++ entry_predictive.py
@@ -1,6 +1,9 @@
 import linear_reg_original
 import json
 
+# if __name__== "__main__":
+
+
 dataset_add = "/home/fidel/mltest/auto-miles-per-gallon.csv"
 feature_colm = ["CYLINDERS", "WEIGHT" , "HORSEPOWER","ACCELERATION", "DISPLACEMENT", "MODELYEAR"]
 label_colm = "MPG"
@@ -11,17 +14,19 @@ def application(data_add, feat_col, label_col, algo):
     response_data=''
     print("received request = ")
 
-    print algo
+    print (algo)
 
     if algo == "linear_reg":
         response_data = linear_reg_original.Linear_reg(dataset_add= data_add, feature_colm=feat_col, label_colm= label_col)
-
+        linear_reg_original.Main()
     else:
-        print "sorry unable to process....."
+        print ("sorry unable to process.....")
 
 
-    print "done"
+    print ("done")
     return iter([response_data])
+
+    print(response_data)
 
 
 application(dataset_add, feature_colm, label_colm, algorithm)
